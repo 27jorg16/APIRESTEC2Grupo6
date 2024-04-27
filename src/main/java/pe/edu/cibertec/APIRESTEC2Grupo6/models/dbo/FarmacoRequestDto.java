8d341f5169976778986bbe8361bf8ab3a70f377a
Juan <juancarlosmazuelos14@gmail.com>
2024-04-26 20:35:25 -0500

    faltaria service

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo6/models/dbo/FarmacoRequestDto.java
@@ -0,0 +1,16 @@
+package pe.edu.cibertec.APIRESTEC2Grupo6.models.dbo;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@Data
+public class FarmacoRequestDto {
+
+    private Integer idfarmaco;
+    private String nombrefarmaco;
+    private String composicion;
+    private Date FechaVencimiento;
+    private List<FarmacoRequestDto> mascotas = new ArrayList<>();
+}
---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo6/repository/farmacoRepository.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.APIRESTEC2Grupo6.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.APIRESTEC2Grupo6.models.bd.Farmaco;
+
+
+@Repository
+public interface farmacoRepository extends JpaRepository<Farmaco, Integer> {
+
+}
