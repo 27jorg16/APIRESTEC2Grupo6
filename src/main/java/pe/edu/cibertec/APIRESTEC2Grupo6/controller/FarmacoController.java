7c70c0c1b05d4d3a1a5723ecc85657ddff60a418
Juan <juancarlosmazuelos14@gmail.com>
2024-04-26 20:15:34 -0500

    controlador

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2Grupo6/controller/FarmacoController.java
@@ -0,0 +1,20 @@
+package pe.edu.cibertec.APIRESTEC2Grupo6.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.APIRESTEC2Grupo6.model.bd.Farmaco;
+import pe.edu.cibertec.APIRESTEC2Grupo6.repository.farmacoRepository;
+import java.util.List;
+
+@RestController
+public class FarmacoController {
+
+    @Autowired
+    private farmacoRepository farmacoRepository;
+
+    @GetMapping("/farmacos")
+    public List<Farmaco> getAllFarmacos() {
+        return farmacoRepository.findAll();
+    }
+}
